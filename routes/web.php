a6a49a361123aa71431c2f1563e352a9561df37e
Jonathan Silva <jhonatrixfull@gmail.com>
2018-08-24 10:02:45 -0500

    comit5

--- routes/web.php
+++ routes/web.php
@@ -15,28 +15,33 @@ Route::get('auth/{provider}', 'Auth\LoginController@redirectToProvider')->name('
 Route::get('auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
 Route::get('lang/{lang}', ['as' => 'lang.switch', 'uses' => 'LanguageController@switchLang']);
 
-Route::group(["middleware" => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'], 'prefix' => LaravelLocalization::setLocale()], function () {
-    Route::get('/home', 'HomeController@index')->name('home');
-    Auth::routes();
-    Route::get('/', function () {
-        return view('welcome');
+
+    Route::group(["middleware" => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath'], 'prefix' => LaravelLocalization::setLocale()], function () {
+        Route::get('/home', 'HomeController@index')->name('home');
+        Auth::routes();
+        Route::get('/', function () {
+            return view('welcome');
+        });
+        Route::group(["middleware" => "auth"], function () {
+            Route::resource("peliculas", "PeliculaController")->except(['store', 'update', 'distroy']);
+            Route::resource("generos", "GeneroController")->except(['store', 'update', 'distroy']);
+            Route::resource("actores", "ActorController")->except(['store', 'update', 'distroy']);
+        });
+    });
+    Route::group(["middleware" => "auth"], function () {
+
+        Route::resource("peliculas","PeliculaController")->only(['store', 'update', 'distroy']);
+        Route::resource("actores","ActorController")->only(['store', 'update', 'distroy']);
+        Route::resource("generos","GeneroController")->only(['store', 'update', 'distroy']);
+
+        Route::post("generos/{id}/restore","GeneroController@restore")->name("generos.restore");
+        Route::post("generos/{id}/trash","GeneroController@trash")->name("generos.trash");
+    
+        Route::post("actores/{id}/restore","ActorController@restore")->name("actores.restore");
+        Route::post("actores/{id}/trash","ActorController@trash")->name("actores.trash");
+    
+        Route::post("peliculas/{id}/restore","PeliculaController@restore")->name("peliculas.restore");
+        Route::post("peliculas/{id}/trash","PeliculaController@trash")->name("peliculas.trash");
     });
 
-Route::group(["middleware"=>"auth"], function () {
-    
-    Route::resource("peliculas","PeliculaController");
-    Route::resource("actores","ActorController");
-    Route::resource("generos","GeneroController");
-    
-    Route::post("generos/{id}/restore","GeneroController@restore")->name("generos.restore");
-    Route::post("generos/{id}/trash","GeneroController@trash")->name("generos.trash");
-
-    Route::post("actores/{id}/restore","ActorController@restore")->name("actores.restore");
-    Route::post("actores/{id}/trash","ActorController@trash")->name("actores.trash");
-
-    Route::post("peliculas/{id}/restore","PeliculaController@restore")->name("peliculas.restore");
-    Route::post("peliculas/{id}/trash","PeliculaController@trash")->name("peliculas.trash");
-
-
-});});
 
